6071ac87c3803eecbc1ae9a631b8fbefad0b4e96
OKOK <OKOK@LAPTOP-JTHJNUU2>
2019-09-04 17:02:00 +0800

    第一次修改

--- testDemo/src/main/java/com/hq/controller/DemoController.java
+++ testDemo/src/main/java/com/hq/controller/DemoController.java
@@ -2,17 +2,20 @@ package com.hq.controller;
 
 public class DemoController {
 
-	
-	public void show() {
-		System.out.println("这是controller");
-	}
-	
-	public void show2() {
-		System.out.println("demo测试操作");
-	}
-	
-	public void show_bai() {
-		System.out.println("这是什么玩意");
-	}
-	
+  public void show() {
+    System.out.println("这是controller");
+  }
+
+  public void show2() {
+    System.out.println("demo测试操作");
+  }
+
+  public void show_bai() {
+    System.out.println("这是什么玩意");
+  }
+
+  public void shaer() {
+    System.out.println("这是什么傻儿");
+  }
+
 }
