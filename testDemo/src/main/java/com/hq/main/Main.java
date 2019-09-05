41674c078361171f6c954fd737cfbe2aa53bc225
OKOK <OKOK@LAPTOP-JTHJNUU2>
2019-09-05 08:36:59 +0800

    第一次修改

---
+++ testDemo/src/main/java/com/hq/main/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Create Date:2019年9月5日
+ */
+package com.hq.main;
+
+/**
+ * <br>Title:TODO 类标题
+ * <br>Description:TODO 类功能描述
+ * <br>Author:张英政
+ * <br>Date:2019年9月5日
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("hollowrod");
+  }
+}
